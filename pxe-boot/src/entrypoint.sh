8d13d2e7f775f5c0787921e0074116face2722a8
Stanislav Odnorog <isodnorog@gmail.com>
2025-08-03 18:11:00 +0300

    make super ultra docker container

--- pxe-boot/src/entrypoint.sh
+++ pxe-boot/src/entrypoint.sh
@@ -3,8 +3,5 @@
 # Generate dnsmasq.conf from template with variables
 envsubst < /tmp/dnsmasq.conf.template > /etc/dnsmasq.conf
 
-# Generate default.ipxe from template
-envsubst < /assets/ipxe/default.ipxe.tpl > /assets/tftpboot/default.ipxe
-
 # Start dnsmasq
 exec dnsmasq -k --conf-file=/etc/dnsmasq.conf -d
